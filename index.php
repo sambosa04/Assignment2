4f2f1c917c917321b9057a7ecbfad323e2e04181
SourceCode.bh <66906105+sambosa04@users.noreply.github.com>
2024-11-24 19:38:42 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- we used pico css  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="overflow-auto">
+    <table class="striped">
+      <thead data-theme="light">
+        <tr>
+          <th scope="col">Year</th>
+          <th scope="col">Semester</th>
+          <th scope="col">The Programs</th>
+          <th scope="col">Nationality</th>
+          <th scope="col">colleges</th>
+          <th scope="col">number_of_students</th>
+    
+        </tr>
+      </thead>
+      <tbody>
+      
+      </tbody>
+      <tfoot>
+      </tfoot>
+    </table>
+   
+</div>
+</body>
+</html>
+
+
