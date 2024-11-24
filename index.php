b3254ee8cc3b11a3d5e81413f70d2c61b4254540
AhmedGTaha <202203742@stu.uob.edu.bh>
2024-11-24 18:45:14 +0200

    Add the PHP segment that retrieve and visualize data to the file

--- index.php
+++ index.php
@@ -22,7 +22,26 @@
         </tr>
       </thead>
       <tbody>
-      
+      <?php 
+         // fetch data from the Bahrain Open Data Portal API and  Parse the JSON response (Data Retrieval)
+         $URL = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
+         $response = file_get_contents($URL);
+         $data = json_decode($response, true);
+    
+         // Print all data in the table (Data Visualization)
+         for($i=0;$i<sizeof($data['results']);$i++)
+         {
+            echo "<tr>";
+            echo "<th scope='row'>" . $data['results'][$i]['year'] . "</th>";
+            echo "<th scope='row'>" . $data['results'][$i]['semester'] . "</th>";
+            echo "<th scope='row'>" . $data['results'][$i]['the_programs'] . "</th>";
+            echo "<th scope='row'>" . $data['results'][$i]['nationality'] . "</th>";
+            echo "<th scope='row'>" . $data['results'][$i]['colleges'] . "</th>";
+            echo "<th scope='row'>" . $data['results'][$i]['number_of_students'] . "</th>";
+            echo "</tr>";
+         }
+        
+       ?>
       </tbody>
       <tfoot>
       </tfoot>
